792dd1fb3e24c4255b44b6614f1fac9f74b7c886
Andrey <ponchikD97@gmail.com>
2018-06-10 16:16:41 +0300

    Update Seventh.cs

--- src/Seventh.cs
+++ src/Seventh.cs
@@ -1,11 +1,8 @@
-﻿namespace RC4
+public static class Seventh
 {
-    public static class Seventh
-    {
-        public static uint ShiftRight(uint zValue, int zShift)
-            => ((zValue >> zShift) | (zValue << (16 - zShift))) & 0x0000FFFF;
+    public static uint ShiftRight(uint zValue, int zShift)
+        => ((zValue >> zShift) | (zValue << (16 - zShift))) & 0x0000FFFF;
 
-        public static uint ShiftLeft(uint zValue, int zShift)
-            => ((zValue << zShift) | (zValue >> (16 - zShift))) & 0x0000FFFF;
-    }
+    public static uint ShiftLeft(uint zValue, int zShift)
+        => ((zValue << zShift) | (zValue >> (16 - zShift))) & 0x0000FFFF;
 }
